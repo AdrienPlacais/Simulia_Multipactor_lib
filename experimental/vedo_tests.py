bbac589eb35741cfbe0344021e62fa9c75c12a14
Adrien P <placais@lpsc.in2p3.fr>
2023-12-15 19:07:48 +0100

    plot of trajectories works

--- experimental/vedo_tests.py
+++ experimental/vedo_tests.py
@@ -6,59 +6,109 @@ Created on Sat Sep 23 13:40:42 2023.
 @author: placais
 
 .. todo::
-    plot trajectories in 3d
-    showcase collision
     histogram impacts
 
 """
 from pathlib import Path
-
-import numpy as np
+from typing import Any
 
 import vedo
-from vedo import Mesh, Points
 
+from multipactor.particle_monitor.particle import Particle
 from multipactor.particle_monitor.particle_monitor import ParticleMonitor
 
-vedo.close()
-plt = vedo.Plotter()
+
+def monitor_to_list_of_particles(particle_monitor: ParticleMonitor,
+                                 indexes: int | slice
+                                 ) -> list[Particle]:
+    """Give Particle objects corresponding to ``indexes``."""
+    parts = list(particle_monitor.values())[indexes]
+    if isinstance(parts, Particle):
+        return [parts]
+    return parts
 
 
-def plot_mesh(stl_file: Path,
-              alpha: float = .3) -> Mesh:
-    mesh = Mesh(stl_file, alpha=alpha)
-    return mesh
+def create_trajectory_plot_objects(particle: Particle) -> vedo.Lines:
+    """Create the plot lines."""
+    lines = vedo.Lines(start_pts=particle.pos[:-1],
+                       end_pts=particle.pos[1:],
+                       lw=7,
+                       )
+    return lines
 
 
-def plot_single_trajectory(positions: np.ndarray,
-                           color: tuple[float, float, float] | None = None
-                           ) -> Points:
-    points = Points(positions, c=color)
-    return points
+def create_collision_point_object(particle: Particle) -> vedo.Points:
+    """Create a collision point."""
+    collision_point = vedo.Point(pos=particle.pos[-1],
+                                 r=12,
+                                 )
+    return collision_point
 
 
-def add_shadows(shadow_positions: tuple[float, float, float],
-                plt: vedo.Plotter,
-                ) -> None:
-    # plt.add_shadows()
-    # for vedo_object in plt:
-    #     vedo_object.add_shadow('x', shadow_positions[0])
-    #     vedo_object.add_shadow('y', shadow_positions[1])
-    #     vedo_object.add_shadow('z', shadow_positions[2])
-    return
+def collision_study(particles: list[Particle],
+                    structure: vedo.Mesh
+                    ) -> tuple[list[vedo.Lines], list[vedo.Points], Any]:
+    """Create trajectory and collision point objects for all particles."""
+    trajectories = [create_trajectory_plot_objects(particle)
+                    for particle in particles]
+
+    collision_color = (255, 125, 255, 255)
+    collision_ids = [find_collision_cell(particle,
+                                         structure,
+                                         collision_color=collision_color)
+                     for particle in particles]
+
+    # Last known point
+    collision_points = [create_collision_point_object(particle)
+                        for particle in particles]
+
+    return trajectories, collision_points, collision_ids
 
 
-basepath = Path("../examples/cst/WR75_reduced/")
-stl_file = Path(basepath, "wr75.stl")
-plt += plot_mesh(stl_file)
+def find_collision_cell(
+        particle: Particle,
+        structure: vedo.Mesh,
+        collision_color: tuple[float, float, float, float] | None = None
+) -> Any:
+    """Find where the trajectory impacts the structure."""
+    # cell = structure.find_cells_along_line(particle.pos[-2],
+    #                                        particle.pos[-1],
+    #                                        tol=0.01)
 
-folder, delimiter = Path(basepath, "Export/3d"), None
-my_particle_monitor = ParticleMonitor(folder, delimiter=delimiter)
+    print("extrapolate position maybe")
+    collision_id = structure.intersect_with_line(particle.pos[-1],
+                                                 particle.pos[-1],
+                                                 return_ids=True,
+                                                 tol=0)
 
-for i in range(1, 100):
-    toplot = my_particle_monitor[i]
-    positions = toplot.pos
-    plt += plot_single_trajectory(positions)
+    if collision_color is not None:
+        my_mesh.cellcolors[collision_id] = collision_color
+    return collision_id
 
-# add_shadows((12., 5., 17.), *plt)
-plt.show(axes=1)
+
+if __name__ == '__main__':
+    vedo.close()
+    veplot = vedo.Plotter()
+
+    # basepath = Path("..", "examples", "cst", "WR75_reduced")
+    # stl_file = Path(basepath, "wr75.stl")
+    # folder, delimiter = Path(basepath, "Export", "3d"), None
+    basepath = Path("/", "home", "placais", "Documents", "Projects", "FCC",
+                    "2023.12_particle_monitor_studies", "data")
+    stl_file = Path(basepath, "swell_1.3GHz.stl")
+    folder, delimiter = Path(basepath, "5_MV_per_m"), None
+
+    # Set up mesh visualisation
+    my_mesh = vedo.load(str(stl_file)).alpha(0.4)
+    veplot += my_mesh
+
+    # Set up trajectories visualisation
+    my_particle_monitor = ParticleMonitor(folder, delimiter=delimiter)
+    to_study = monitor_to_list_of_particles(my_particle_monitor, slice(0, 10))
+    trajectories, collision_points, _ = collision_study(to_study, my_mesh)
+    veplot += trajectories + collision_points
+
+    # Find intersections
+    # my_mesh.intersect_with_lines
+
+    veplot.show()
