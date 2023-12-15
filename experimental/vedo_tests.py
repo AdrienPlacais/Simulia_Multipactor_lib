84a9694bd71ecd49aefbdb1a53e81dbca63aecbb
Adrien P <placais@lpsc.in2p3.fr>
2023-12-15 10:09:17 +0100

    keep files to try to visualise stl

---
+++ experimental/vedo_tests.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 23 13:40:42 2023.
+
+@author: placais
+
+.. todo::
+    plot trajectories in 3d
+    showcase collision
+    histogram impacts
+
+"""
+import os.path
+
+import numpy as np
+
+import vedo
+from vedo import Mesh, Points
+
+from multipactor.particle_monitor.particle_monitor import ParticleMonitor
+
+vedo.close()
+plt = vedo.Plotter()
+
+
+def plot_mesh(path: str,
+              alpha: float = .3) -> Mesh:
+    mesh = Mesh(stl_file, alpha=alpha)
+    return mesh
+
+
+def plot_single_trajectory(positions: np.ndarray,
+                           color: tuple[float, float, float] | None = None
+                           ) -> Points:
+    points = Points(positions, c=color)
+    return points
+
+
+def add_shadows(shadow_positions: tuple[float, float, float],
+                plt: vedo.Plotter,
+                ) -> None:
+    # plt.add_shadows()
+    # for vedo_object in plt:
+    #     vedo_object.add_shadow('x', shadow_positions[0])
+    #     vedo_object.add_shadow('y', shadow_positions[1])
+    #     vedo_object.add_shadow('z', shadow_positions[2])
+    return
+
+
+basepath = "../examples/cst/WR75_reduced/"
+stl_file = os.path.join(basepath, "wr75.stl")
+plt += plot_mesh(stl_file)
+
+folder, delimiter = os.path.join(basepath, "Export/3d"), None
+my_particle_monitor = ParticleMonitor(folder, delimiter=delimiter)
+
+for i in range(1, 100):
+    toplot = my_particle_monitor[i]
+    positions = toplot.pos
+    plt += plot_single_trajectory(positions)
+
+# add_shadows((12., 5., 17.), *plt)
+plt.show(axes=1)
