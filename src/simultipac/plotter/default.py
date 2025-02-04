d301425f0bc1c7a8a9381c46f5b8efd8b151a2e7
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2025-02-04 09:26:30 +0100

    do not separate 2d and 3d plotters

--- src/simultipac/plotter/default.py
+++ src/simultipac/plotter/default.py
@@ -1,18 +1,22 @@
-"""Define a plotter adapted to 2D data."""
+"""Define a default plotter."""
 
 from collections.abc import Sequence
+from typing import Any
 
 import matplotlib.pyplot as plt
+import vedo
 from matplotlib.axes import Axes
 
 from simultipac.plotter.plotter import Plotter
 
 
-class MatplotlibPlotter(Plotter):
-    """An object for 2D data."""
-
-    supports_3d = False
+class DefaultPlotter(Plotter):
+    """An object using maptlotlib for 2D, Vedo for 3D."""
 
     def plot(self, *args, **kwargs) -> Axes | Sequence[Axes]:
         """Plot 2D data."""
+        raise NotImplementedError
+
+    def plot_3d(self, *args, **kwargs) -> Any:
+        """Create a 3D plot."""
         raise NotImplementedError
--- src/simultipac/plotter/plotter.py
+++ src/simultipac/plotter/plotter.py
@@ -1,6 +1,6 @@
 """Define the generic :class:`Plotter`."""
 
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import Any
 
 import pandas as pd
@@ -9,8 +9,10 @@ import pandas as pd
 class Plotter(ABC):
     """An object used to plot data."""
 
-    supports_3d: bool
-
+    @abstractmethod
     def plot(self, data: pd.DataFrame, *args, **kwargs) -> Any:
         """Plot data and return axes object."""
-        raise NotImplementedError
+
+    @abstractmethod
+    def plot_3d(self, *args, **kwargs) -> Any:
+        """Create a 3D plot."""
