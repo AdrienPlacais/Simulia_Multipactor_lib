bd80be47302d74c23d282cd9df2f17ea14ba1ae4
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2025-02-04 12:44:27 +0100

    small markdown dictionary

--- src/simultipac/constants.py
+++ src/simultipac/constants.py
@@ -3,3 +3,10 @@
 clight = 299_792_458.0
 clight_in_mm_per_ns = clight * 1e-6
 qelem = 1.6021766e-19
+
+markdown: dict[str, str] = {
+    "alpha": r"$\alpha$ [ns$^{-1}$]",
+    "e_acc": "$E_{acc}$ [V/m]",
+    "population": "$n_e$",
+    "time": "$t$ [ns]",
+}
