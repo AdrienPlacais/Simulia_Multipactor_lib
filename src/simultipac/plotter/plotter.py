d1e703195f746ed0c24d04997336cd0af19f25a2
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2025-02-04 13:10:31 +0100

    consistent Plotter.plot args

--- src/simultipac/plotter/plotter.py
+++ src/simultipac/plotter/plotter.py
@@ -10,8 +10,46 @@ class Plotter(ABC):
     """An object used to plot data."""
 
     @abstractmethod
-    def plot(self, data: pd.DataFrame, x: str, y: str, *args, **kwargs) -> Any:
-        """Plot data and return axes object."""
+    def plot(
+        self,
+        data: pd.DataFrame,
+        x: str,
+        y: str,
+        grid: bool = True,
+        axes: Any | None = None,
+        xlabel: str | None = None,
+        ylabel: str | None = None,
+        label: str | None = None,
+        **kwargs,
+    ) -> Any:
+        """Plot 2D data.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            Holds all data to plot.
+        x, y : str
+            Name of column in ``data`` for x/y.
+        grid : bool, optional
+            If grid should be plotted. Default is True.
+        axes : Any | None, optional
+            Axes to re-use, if provided. The default is None (plot on new
+            axis).
+        xlabel, ylabel : str | None, optional
+            Name of the labels. If not provided, we use the markdown equivalent
+            of x/y, if defined in :data:`.markdown`.
+        label : str | None, optional
+            If provided, overrides the legend. Useful when several simulations
+            are shown on the same plot.
+        kwargs :
+            Other keyword passed to the actual plot method.
+
+        Returns
+        -------
+        Any
+            Axis objects.
+
+        """
 
     @abstractmethod
     def plot_3d(self, *args, **kwargs) -> Any:
