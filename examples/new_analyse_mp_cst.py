11ee0dfb6215502b76eca4dccdb63fb4b5f3b2dc
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2025-02-04 09:53:05 +0100

    updated future interface

--- examples/new_analyse_mp_cst.py
+++ examples/new_analyse_mp_cst.py
@@ -2,23 +2,20 @@
 
 from pathlib import Path
 
-from simultipac.cst.simulation_results import CSTResults
 from simultipac.simulation_results.simulations_results import (
-    SimulationResultsFactory,
+    SimulationsResults,
+    SimulationsResultsFactory,
 )
 
-factory = SimulationResultsFactory()
-results: CSTResults = factory.create(
-    tool="CST",
+factory = SimulationsResultsFactory("CST")
+results: SimulationsResults = factory.create(
     folderpath=Path("cst/Export_Parametric"),
 )
 idx_to_plot = (0, 5, 10)
 axes = results.plot(x="time", y="population", idx_to_plot=idx_to_plot)
 
 results.fit_alpha(fitting_periods=5, model="classic")
-results.plot(
-    x="time", y="alpha", idx_to_plot=idx_to_plot, same_colors=True, axes=axes
-)
+results.plot(x="time", y="alpha", idx_to_plot=idx_to_plot, axes=axes)
 results.plot(x="e_acc", y="alpha")
 results.save(
     "growth.txt",
--- examples/new_analyse_mp_spark.py
+++ examples/new_analyse_mp_spark.py
@@ -5,13 +5,12 @@ from pathlib import Path
 import numpy as np
 
 from simultipac.simulation_results.simulations_results import (
-    SimulationResultsFactory,
+    SimulationsResults,
+    SimulationsResultsFactory,
 )
-from simultipac.spark3d.simulation_results import Spark3DResults
 
-factory = SimulationResultsFactory()
-results: Spark3DResults = factory.create(
-    tool="SPARK3D",
+factory = SimulationsResultsFactory("SPARK3D")
+results: SimulationsResults = factory.create(
     filepath=Path("spark/time_results.csv"),
     e_acc=np.linspace(1e6, 3e7, 30),
     freq=1.30145,
@@ -20,9 +19,7 @@ idx_to_plot = (0, 5, 10)
 axes = results.plot(x="time", y="population", idx_to_plot=idx_to_plot)
 
 results.fit_alpha(fitting_periods=20, model="classic")
-results.plot(
-    x="time", y="alpha", idx_to_plot=idx_to_plot, same_colors=True, axes=axes
-)
+results.plot(x="time", y="alpha", idx_to_plot=idx_to_plot, axes=axes)
 results.plot(x="e_acc", y="alpha")
 results.save(
     "growth.txt",
