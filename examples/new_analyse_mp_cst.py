b08b66e21448850d86018c0e462a88f4f6f82619
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2025-02-05 17:45:59 +0100

    adapted new_* scripts for better showcasing

--- examples/new_analyse_mp_cst.py
+++ examples/new_analyse_mp_cst.py
@@ -7,21 +7,31 @@ from simultipac.simulation_results.simulations_results import (
     SimulationsResultsFactory,
 )
 
-factory = SimulationsResultsFactory("CST")
-results: SimulationsResults = factory.create(
-    master_folder=Path("cst/Export_Parametric"),
-)
-idx_to_plot = (0, 5, 90)
-axes = results.plot(x="time", y="population", idx_to_plot=idx_to_plot)
+if __name__ == "__main__":
+    factory = SimulationsResultsFactory("CST", freq_ghz=1.30145)
+    results: SimulationsResults = factory.create(
+        master_folder=Path("cst/Export_Parametric"),
+    )
+    idx_to_plot = (0, 5, 90)
+    axes = results.plot(
+        x="time", y="population", idx_to_plot=idx_to_plot, alpha=0.7
+    )
 
-results.fit_alpha(fitting_periods=5, model="classic")
-results.plot(x="time", y="alpha", idx_to_plot=idx_to_plot, axes=axes)
-results.plot(x="e_acc", y="alpha")
-results.save(
-    "growth.txt",
-    *(
-        "e_acc",
-        "alpha",
-    ),
-    delimiter="\t",
-)
+    results.fit_alpha(fitting_periods=5, minimum_final_number_of_electrons=5)
+    results.plot(
+        x="time",
+        y="modelled_population",
+        idx_to_plot=idx_to_plot,
+        axes=axes,
+        lw=3,
+        ls="--",
+    )
+    # results.plot(x="e_acc", y="alpha")
+    # results.save(
+    #     "growth.txt",
+    #     *(
+    #         "e_acc",
+    #         "alpha",
+    #     ),
+    #     delimiter="\t",
+    # )
--- examples/new_analyse_mp_spark.py
+++ examples/new_analyse_mp_spark.py
@@ -9,23 +9,34 @@ from simultipac.simulation_results.simulations_results import (
     SimulationsResultsFactory,
 )
 
-factory = SimulationsResultsFactory("SPARK3D")
-results: SimulationsResults = factory.create(
-    filepath=Path("spark/time_results.csv"),
-    e_acc=np.linspace(1e6, 3e7, 30),
-    freq=1.30145,
-)
-idx_to_plot = (0, 5, 10)
-axes = results.plot(x="time", y="population", idx_to_plot=idx_to_plot)
+if __name__ == "__main__":
+    factory = SimulationsResultsFactory("SPARK3D", freq_ghz=1.30145)
+    results: SimulationsResults = factory.create(
+        filepath=Path("spark/time_results.csv"),
+        e_acc=np.linspace(1e6, 3e7, 30),
+        freq=1.30145,
+    )
+    idx_to_plot = (0, 15, 25)
+    axes = results.plot(
+        x="time", y="population", idx_to_plot=idx_to_plot, alpha=0.7
+    )
 
-results.fit_alpha(fitting_periods=20, model="classic")
-results.plot(x="time", y="alpha", idx_to_plot=idx_to_plot, axes=axes)
-results.plot(x="e_acc", y="alpha")
-results.save(
-    "growth.txt",
-    *(
-        "e_acc",
-        "alpha",
-    ),
-    delimiter="\t",
-)
+    results.fit_alpha(fitting_periods=20)
+    results.plot(
+        x="time",
+        y="modelled_population",
+        idx_to_plot=idx_to_plot,
+        axes=axes,
+        lw=3,
+        ls="--",
+    )
+    axes.set_yscale("log")
+    # results.plot(x="e_acc", y="alpha")
+    # results.save(
+    #     "growth.txt",
+    #     *(
+    #         "e_acc",
+    #         "alpha",
+    #     ),
+    #     delimiter="\t",
+    # )
