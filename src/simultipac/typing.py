4075913355912d5b67be6b94a7c03e2e81a1e58d
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2025-02-20 13:18:18 +0100

    defined types for the 3D plots

--- src/simultipac/typing.py
+++ src/simultipac/typing.py
@@ -2,12 +2,30 @@
 
 from typing import Literal
 
-#: :class:`.SimulationResult` attributes stored as float.
+#: :class:`.SimulationResults` attributes stored as float.
 DATA_0D = ("id", "e_acc", "p_rms", "alpha")
-#: :class:`.SimulationResult` attributes stored as float.
+#: :class:`.SimulationResults` attributes stored as float.
 DATA_0D_t = Literal["id", "e_acc", "p_rms", "alpha"]
 
-#: :class:`.SimulationResult` attributes stored as 1D arrays.
+#: :class:`.SimulationResults` attributes stored as 1D arrays.
 DATA_1D = ("time", "population", "modelled_population")
 #: :class:`.SimulationResult` attributes stored as 1D arrays.
 DATA_1D_t = Literal["time", "population", "modelled_population"]
+
+#: :class:`.CSTSimulationResults` attributes if :class:`.ParticleMonitor` was
+# defined
+PARTICLE_0D = ("emission_energy", "collision_energy", "collision_angle")
+#: :class:`.CSTSimulationResults` attributes if :class:`.ParticleMonitor` was
+# defined
+PARTICLE_0D_t = Literal[
+    "emission_energy", "collision_energy", "collision_angle"
+]
+
+#: :class:`.CSTSimulationResults` attributes if :class:`.ParticleMonitor` was
+# defined
+PARTICLE_3D = ("trajectory", "collision_distribution", "emission_distribution")
+#: :class:`.CSTSimulationResults` attributes if :class:`.ParticleMonitor` was
+# defined
+PARTICLE_3D_t = Literal[
+    "trajectory", "collision_distribution", "emission_distribution"
+]
