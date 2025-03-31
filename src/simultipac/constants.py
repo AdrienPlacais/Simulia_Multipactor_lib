645f9b77e2034def5821382638865552cd4082e1
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2025-03-31 21:33:41 +0200

    added docstring to markdown dictionary

--- src/simultipac/constants.py
+++ src/simultipac/constants.py
@@ -4,6 +4,7 @@ clight = 299_792_458.0
 clight_in_mm_per_ns = clight * 1e-6
 qelem = 1.6021766e-19
 
+#: Associates name of variables in the library to markdown formatted strings
 markdown: dict[str, str] = {
     "alpha": r"$\alpha$ [ns$^{-1}$]",
     "collision_angle": r"Impact angle $\theta$ [deg]",
