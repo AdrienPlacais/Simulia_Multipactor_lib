90d59b6e4a4c6952f768d2f2c44a97f551d737ac
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2025-04-14 11:10:16 +0200

    removed debug messages from Singleton

--- src/simultipac/util/singleton.py
+++ src/simultipac/util/singleton.py
@@ -1,6 +1,5 @@
 """Define a base singleton pattern class."""
 
-import logging
 from abc import ABC
 
 
@@ -12,9 +11,6 @@ class SingletonMeta(type, ABC):
     def __call__(cls, *args, **kwargs):
         """Create instance only if not already exists."""
         if cls not in cls._instances:
-            logging.info("Called __call__ (None)")
             cls._instances[cls] = super().__call__(*args, **kwargs)
-        else:
-            logging.info("Called __call__ (not None)")
 
         return cls._instances[cls]
